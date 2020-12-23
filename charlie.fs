47018e07c4a80ce046ddf2e34bad5f7db94334a2
Namdak Tonpa <maxim@synrc.com>
2020-12-23 04:26:40 +0200

    14M msg/s

--- charlie.fs
+++ charlie.fs
@@ -87,12 +87,5 @@ module SharedActor =
       for i in 0 .. 14000000 do
           u.Post <| Add i
 
-  // 14M msgs/sec demo for F# .NET 5.0 target for 8700
-
-  // $ time bin/Debug/net5.0/charlie
-  // real    0m1.109s
-  // user    0m1.063s
-  // sys     0m0.047s
-
   [<EntryPoint>]
   let main args = args |> eval |> ret
